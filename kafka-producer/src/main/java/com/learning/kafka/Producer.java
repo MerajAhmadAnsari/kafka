93b212d9d1820fc439b709bb08becefed80854e0
Meraj Ahmad Ansari <MerajAhmadAnsari@users.noreply.github.com>
2023-08-18 02:18:29 +0530

    initial skeleton code

---
+++ kafka-producer/src/main/java/com/learning/kafka/Producer.java
@@ -0,0 +1,13 @@
+package com.learning.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class Producer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ kafka-producer/src/test/java/com/learning/kafka/ProducerTest.java
@@ -0,0 +1,20 @@
+package com.learning.kafka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProducerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
