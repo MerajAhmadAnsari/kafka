ac65004c8f5a8a5a39f399f3fab610b3499288df
Meraj Ahmad Ansari <MerajAhmadAnsari@users.noreply.github.com>
2023-08-18 02:40:04 +0530

    producer callback added

--- kafka-producer/src/main/java/com/learning/kafka/Producer.java
+++ kafka-producer/src/main/java/com/learning/kafka/Producer.java
@@ -1,9 +1,14 @@
 package com.learning.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Properties;
 import java.util.Random;
 
@@ -24,7 +29,18 @@ public class Producer {
         //send data
         String msg = "SDK : " + new Random().nextInt();
         ProducerRecord<String, String> record = new ProducerRecord<>("first-topic", msg);
-        producer.send(record);
+        producer.send(record, (RecordMetadata recordMetadata, Exception e) -> {
+                    if (null == e) {
+                        System.out.println("Partition : " + recordMetadata.partition());
+                        System.out.println("Topic : " + recordMetadata.topic());
+                        System.out.println("Offset : " + recordMetadata.offset());
+                        System.out.println("Timestamp : " + LocalDateTime.ofInstant(Instant.ofEpochMilli(recordMetadata.timestamp()), ZoneId.systemDefault()));
+                    } else {
+                        System.out.println("Error occurred " + e);
+                    }
+
+                }
+        );
 
         System.out.println("Message " + msg + " sent!!");
 
