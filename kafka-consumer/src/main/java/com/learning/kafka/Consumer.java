41c55422e65d9300b3fa6726f3ed5cd6b3033a9c
Meraj Ahmad Ansari <MerajAhmadAnsari@users.noreply.github.com>
2023-08-18 04:10:55 +0530

    Shutdown hook added for consumer

--- kafka-consumer/src/main/java/com/learning/kafka/Consumer.java
+++ kafka-consumer/src/main/java/com/learning/kafka/Consumer.java
@@ -1,8 +1,10 @@
 package com.learning.kafka;
 
+import jdk.jshell.spi.ExecutionControlProvider;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Arrays;
@@ -25,20 +27,43 @@ public class Consumer {
         //Create Consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
+        // handle graceful exit
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    System.out.println("Handling shutdown, waking up consumer...");
+                    consumer.wakeup();
+                    try {
+                        mainThread.join();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+        );
         //Subscribe to topic
         consumer.subscribe(Arrays.asList("first-topic"));
 
         //Poll for data
-        while (true) {
-            System.out.println("Waiting for new messages...");
+        try{
+            while (true) {
+                System.out.println("Waiting for new messages...");
 
-            ConsumerRecords<String, String> records = consumer.poll(1000);
-            for( ConsumerRecord<String, String> singleRecord : records) {
-                System.out.println("Key : " + singleRecord.key());
-                System.out.println("Value : " + singleRecord.value());
-                System.out.println("Offset : " + singleRecord.offset());
-                System.out.println("Partition : " + singleRecord.partition());
+                ConsumerRecords<String, String> records = consumer.poll(1000);
+                for (ConsumerRecord<String, String> singleRecord : records) {
+                    System.out.println("Key : " + singleRecord.key());
+                    System.out.println("Value : " + singleRecord.value());
+                    System.out.println("Offset : " + singleRecord.offset());
+                    System.out.println("Partition : " + singleRecord.partition());
+                }
             }
+        }catch(WakeupException e){
+            System.out.println("Consumer is going to shutdown");
         }
+        catch(Exception e){
+            System.out.println("Generic error : " + e.getMessage());
+        }
+        finally {
+            consumer.close();
+        }
+
     }
 }
